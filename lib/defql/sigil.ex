fa015bf796bffb134f933eedd7f165953f84b95c
Michał Kalbarczyk <fazibear@gmail.com>
2017-03-23 16:09:45 +0100

    Add missing docs

--- lib/defql/sigil.ex
+++ lib/defql/sigil.ex
@@ -1,4 +1,7 @@
 defmodule Defql.Sigil do
+  @moduledoc """
+  This module define sigil for querying database.
+  """
   alias Defql.Connection
 
   def sigil_q(query, []), do: Connection.query(query, [])
